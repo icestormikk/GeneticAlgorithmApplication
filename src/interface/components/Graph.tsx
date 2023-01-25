09848e99e746d0b6501dfe5a7f10fd18d29ac3a1
icestormikk <jigalow.pavel2015@yandex.ru>
2023-01-25 13:20:04 +0300

    added new function + restructuring

--- src/interface/components/Graph.tsx
+++ src/interface/components/Graph.tsx
@@ -8,6 +8,22 @@ import {ReduxLinkObject} from '../redux/extensions/ReduxLinkObject';
 import {
   getRandomNumber,
 } from '../../geneticAlgorithm/operators/RecombinationOperators';
+import SpriteText from 'three-spritetext';
+import {ExtendedNodeObject} from '../redux/extensions/ExtendedNodeObject';
+import {ExtendedLinkObject} from '../redux/extensions/ExtendedLinkObject';
+
+/**
+ * Accepts an object and returns a human-readable representation of its values
+ * @param {T} object the object whose values need to be translated
+ * @return {string} human-readable representation of object values
+ * @template T
+ */
+function beautify<T>(object: T) : string {
+  return JSON.stringify(object)
+      .replace(/["{}\s]/ig, '')
+      .replace(/,/ig, '\n')
+      .replace(/:/ig, ': ');
+}
 
 export const createNewLink = (
     fromNode: ReduxNodeObject, toNode: ReduxNodeObject,
@@ -19,6 +35,8 @@ export const createNewLink = (
     value: {
       distance: getRandomNumber(0, 100),
       cost: getRandomNumber(50, 100),
+      broken: Math.random() > 0.5,
+      isPath: Math.random() > 0.5,
     },
   };
 };
@@ -33,7 +51,13 @@ function Graph() {
   const dispatch = useAppDispatch();
   const [width, setWidth] = React.useState(window.innerWidth);
   const [height, setHeight] = React.useState(window.innerHeight);
-  const {nodes, links, selectedNodes} = useAppSelector((state) => state.graph);
+  const {
+    nodes,
+    links,
+    selectedNodes,
+    path,
+    isAdditionalElementsShow,
+  } = useAppSelector((state) => state.graph);
   const initData = React.useMemo(() => {
     const refactoredNodes = nodes.map((el) => JSON.parse(JSON.stringify(el)));
     const refactoredLinks = links.map((el) => JSON.parse(JSON.stringify(el)));
@@ -65,20 +89,52 @@ function Graph() {
     width={width}
     height={height}
     graphData={initData}
-    linkDirectionalParticles={1}
     nodeColor={(node) =>
       selectedNodes.find((el) => el.id === node.id) ? 'white' : 'yellow'
     }
     nodeLabel={(node) => `node-${node.id}`}
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
     // @ts-ignore
-    onNodeRightClick={(node: ExtendedNodeObject) => handleNodeRightClick(node)}
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    onNodeRightClick={(node: ExtendedNodeObject) =>
+      handleNodeRightClick(node)
+    }
+    /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
     // @ts-ignore
     onNodeClick={(node: ExtendedNodeObject) => handleNodeLeftClick(node)}
+    linkThreeObjectExtend={true}
+    /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+    // @ts-ignore
+    linkThreeObject={(link: ExtendedLinkObject) => {
+      const sprite = new SpriteText(
+          isAdditionalElementsShow ? beautify(link.value) : ' ',
+      );
+      sprite.color = link.value.broken ? 'red' : 'green';
+      sprite.fontFace = 'Arial';
+      sprite.textHeight = 1.5;
+      return sprite;
+    }}
+    /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+    // @ts-ignore
+    linkPositionUpdate={(sprite, {start, end}) => {
+      const strings: [string, string, string] = ['x', 'y', 'z'];
+      const middlePos = Object.assign({}, ...strings.map((c) => ({
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        [c]: start[c] + (end[c] - start[c]) / 2,
+      })));
+
+      Object.assign(sprite.position, middlePos);
+    }}
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    linkColor={(link: ExtendedLinkObject) =>
+      path.find((el) => el.id === link.id) ? 'lime' : 'red'
+    }
+    linkDirectionalParticles={1}
     linkDirectionalArrowLength={3.5}
     linkDirectionalArrowRelPos={1}
-    linkCurvature={0.0}
+    linkCurvature={0.2}
+    linkWidth={1}
   />;
 }
 
