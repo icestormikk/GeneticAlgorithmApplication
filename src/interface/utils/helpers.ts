96d743a8410ce61bdc05d96f26fd25e9e37bb40b
icestormikk <jigalow.pavel2015@yandex.ru>
2023-01-26 01:29:02 +0300

    taking out functions useful for the project into a separate file

---
+++ src/interface/utils/helpers.ts
@@ -0,0 +1,24 @@
+/**
+ * Accepts an object and returns a human-readable representation of its values
+ * @param {T} object the object whose values need to be translated
+ * @return {string} human-readable representation of object values
+ * @template T
+ */
+export function beautify<T>(object: T) : string {
+  return JSON.stringify(object)
+      .replace(/["{}\s]/ig, '')
+      .replace(/,/ig, '\n')
+      .replace(/:/ig, ': ');
+}
+
+/**
+ * Accepts a string as an uuid and generates a number that is the sum of
+ * the character codes in the passed string
+ * @param {string} uuid a sequence of characters that is the uuid of an objeсt
+ * @return {number} generated number
+ */
+export function buildNameFromUUID(uuid: string) : number {
+  return [...uuid.replace(/-/ig, '')].reduce((a, b) =>
+    a + b.charCodeAt(0), 0,
+  );
+}
