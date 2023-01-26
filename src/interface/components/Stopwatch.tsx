649199066a21dbc390f13bc3546d1d69a54565a4
icestormikk <jigalow.pavel2015@yandex.ru>
2023-01-27 01:37:09 +0300

    added a customizable stopwatch

---
+++ src/interface/components/Stopwatch.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+/**
+ * A stopwatch that allows you to measure the
+ * duration of an action
+ * @constructor
+ */
+function Stopwatch() {
+  const [time, setTime] = React.useState(0);
+  const [isLaunched, setIsLaunched] = React.useState(false);
+
+  React.useEffect(() => {
+    let interval: NodeJS.Timeout;
+    if (isLaunched) {
+      interval = setInterval(() => {
+        setTime((prevState) => prevState + 10);
+      }, 10);
+    } else {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isLaunched]);
+
+  return (
+    <div className="flex">
+      <div className="flex flex-row">
+        <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{('0' + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div className="buttons">
+        <button onClick={() => setIsLaunched(true)}>Start</button>
+        <button onClick={() => setIsLaunched(false)}>Stop</button>
+        <button onClick={() => setTime(0)}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Stopwatch;
