79320fc68cf7f0f245dd932eaea6042195064f9f
icestormikk <jigalow.pavel2015@yandex.ru>
2023-02-06 00:41:31 +0300

    added an element to display information about existing nodes

---
+++ src/interface/components/NodeEntity/NodeEntity.lazy.tsx
@@ -0,0 +1,15 @@
+import React, {lazy, Suspense} from 'react';
+
+const LazyNodeEntity = lazy(() => import('./NodeEntity'));
+
+interface NodeEntityProps {
+  id: string
+}
+
+const NodeEntity = (props: JSX.IntrinsicAttributes & NodeEntityProps) => (
+  <Suspense fallback={null}>
+    <LazyNodeEntity {...props} />
+  </Suspense>
+);
+
+export default NodeEntity;
---
+++ src/interface/components/NodeEntity/NodeEntity.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import NodeEntity from './NodeEntity';
+
+describe('<NodeEntity />', () => {
+  test('it should mount', () => {
+    render(<NodeEntity id={'1'}/>);
+
+    const nodeEntity = screen.getByTestId('NodeEntity');
+
+    expect(nodeEntity).toBeInTheDocument();
+  });
+});
