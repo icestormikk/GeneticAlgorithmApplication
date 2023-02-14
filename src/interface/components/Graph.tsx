e4e62a8eefc07a669decbd22a0a7e4fbc513989f
icestormikk <jigalow.pavel2015@yandex.ru>
2023-02-14 18:05:10 +0300

    updated

--- src/interface/components/Graph.tsx
+++ src/interface/components/Graph.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// noinspection TypeScriptValidateTypes
+
 import React from 'react';
 import ForceGraph from 'react-force-graph-3d';
 import {generateUUID} from 'three/src/math/MathUtils';
@@ -6,7 +9,7 @@ import {ReduxNodeObject} from '../redux/extensions/ReduxNodeObject';
 import {ReduxLinkObject} from '../redux/extensions/ReduxLinkObject';
 import {
   getRandomNumber,
-} from '../../geneticAlgorithm/operators';
+} from '../../geneticAlgorithm/functions/arrayhelper';
 import SpriteText from 'three-spritetext';
 import {ExtendedNodeObject} from '../redux/extensions/ExtendedNodeObject';
 import {ExtendedLinkObject} from '../redux/extensions/ExtendedLinkObject';
@@ -41,11 +44,34 @@ function Graph() {
   const dispatch = useAppDispatch();
   const [width, setWidth] = React.useState(window.innerWidth);
   const [height, setHeight] = React.useState(window.innerHeight);
-  const {isAdditionalElementsShow} = useAppSelector((state) => state.graph);
+  const {
+    isAdditionalElementsShow, foundPath,
+  } = useAppSelector((state) => state.graph);
   const links = useAppSelector((state) => state.links.items);
   const nodes = useAppSelector((state) => state.nodes.items);
   const selectedNodes = useAppSelector((state) => state.nodes.selectedItems
       .map((el) => JSON.parse(JSON.stringify(el))),
+  );
+  const suitableLinks = React.useMemo(
+      () => {
+        if (!foundPath) {
+          return undefined;
+        }
+
+        const result: Array<ExtendedLinkObject> = [];
+        for (let i = 0; i < foundPath.length - 1; i++) {
+          const item = links.find((el) =>
+            el.source === foundPath[i] && el.target === foundPath[i + 1],
+          );
+
+          if (item) {
+            result.push(item);
+          }
+        }
+
+        return result;
+      },
+      [links, foundPath],
   );
 
   const initData = React.useMemo(() => {
@@ -94,21 +120,17 @@ function Graph() {
     height={height}
     graphData={initData}
     nodeColor={(node) =>
-      selectedNodes.find((el) => el.id === node.id) ? 'white' : 'yellow'
+     selectedNodes.find((el) => el.id === node.id) ? 'white' : 'yellow'
     }
-    /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
     // @ts-ignore
     nodeLabel={(node: ExtendedNodeObject) => node.label }
-    /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
     // @ts-ignore
     onNodeRightClick={(node: ExtendedNodeObject) =>
       handleNodeRightClick(node)
     }
-    /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
     // @ts-ignore
     onNodeClick={(node: ExtendedNodeObject) => handleNodeLeftClick(node)}
     linkThreeObjectExtend={true}
-    /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
     // @ts-ignore
     linkThreeObject={(link: ExtendedLinkObject) => {
       const sprite = new SpriteText(
@@ -119,12 +141,10 @@ function Graph() {
       sprite.textHeight = 1.5;
       return sprite;
     }}
-    /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
     // @ts-ignore
     linkPositionUpdate={(sprite, {start, end}) => {
       const strings: [string, string, string] = ['x', 'y', 'z'];
       const middlePos = Object.assign({}, ...strings.map((c) => ({
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         [c]: start[c] + (end[c] - start[c]) / 2,
       })));
@@ -136,6 +156,18 @@ function Graph() {
     linkDirectionalArrowRelPos={1}
     linkCurvature={0.2}
     linkWidth={0.5}
+    // @ts-ignore
+    linkColor={(link: ExtendedLinkObject) => {
+      if (!suitableLinks) {
+        return 'red';
+      }
+
+      const isSuitable = suitableLinks.find((el) =>
+        link.id === el.id,
+      );
+
+      return isSuitable ? 'green' : 'red';
+    }}
   />;
 }
 
