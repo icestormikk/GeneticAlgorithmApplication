3553aa390554004ddd56f5d5ccee54ee1d703c6b
icestormikk <jigalow.pavel2015@yandex.ru>
2023-03-09 23:47:57 +0300

    added a method for converting a two-element array into a Pair object

--- src/geneticAlgorithm/domain/Pair.ts
+++ src/geneticAlgorithm/domain/Pair.ts
@@ -16,4 +16,14 @@ export class Pair<T, R> {
     this.first = first;
     this.second = second;
   }
+
+  static fromArray<T>(source: Array<T>) : Pair<T, T> {
+    if (source.length !== 2) {
+      throw new Error(
+          'It is impossible to create a pair from an array of elements with a length not equal to two'
+      )
+    }
+
+    return new Pair(source[0], source[1])
+  }
 }
